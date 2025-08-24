9fb3e4793813387965e36db81f376615657206b7
Nikolay Venediktov <Mr.venik2004@mail.ru>
2025-08-24 18:44:12 +0300

    update primary color

---
+++ EasyEnglish.UI/Consts.cs
@@ -0,0 +1,14 @@
+﻿using MudBlazor;
+
+namespace EasyEnglish.UI;
+
+internal static class Consts
+{
+    internal static readonly MudTheme MudTheme = new()
+    {
+        PaletteLight = new PaletteLight
+        {
+            Primary = "#274edb"
+        }
+    };
+}
