66a66edb3d5a3b0b1e5926ce9c4723133d618c3f
Nikolay Venediktov <Mr.venik2004@mail.ru>
2025-10-23 19:24:21 +0300

    oops... add migration

---
+++ EasyEnglish.Persistence/Migrations/20251023055148_Add_Column_IsLearned_For_Table_CardCollection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyEnglish.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Column_IsLearned_For_Table_CardCollection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLearned",
+                table: "CardCollection",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLearned",
+                table: "CardCollection");
+        }
+    }
+}
